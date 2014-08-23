abf9cfae7e27ac1699cb41024de5873cfa44a421
Kiril_Janchev <you@example.com>
2014-08-23 16:48:13 +0300

    Minor changes

--- MyDay/src/main/java/lv/localhost/MyDay/Controllers/RegistrationValidation.java
+++ MyDay/src/main/java/lv/localhost/MyDay/Controllers/RegistrationValidation.java
@@ -9,20 +9,32 @@ import lv.localhost.MyDay.common.DBException;
 
 @Component("registrationValidator")
 public class RegistrationValidation {
-  public boolean supports(Class<?> klass) {
-    return Account.class.isAssignableFrom(klass);
-  }
+	public boolean supports(Class<?> klass) {
+		return Account.class.isAssignableFrom(klass);
+	}
 
-  public void validate(Object target, Errors errors) {
-	  Account registration = (Account) target;
-    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "login",
-        "NotEmpty.registration.login",
-        "User Name must not be Empty.");
-    String login = registration.getLogin();
-    if ((login.length()) > 50) {
-      errors.rejectValue("login",
-          "lengthOfUser.registration.login",
-          "User Name must not more than 50 characters.");
-    }
-  }
+	public void validate(Object target, Errors errors) {
+		Account registration = (Account) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "login",
+				"NotEmpty.registration.login", "User Name must not be Empty.");
+
+		ValidationUtils
+				.rejectIfEmptyOrWhitespace(errors, "password",
+						"NotEmpty.registration.password",
+						"Password must not be Empty.");
+
+		String login = registration.getLogin();
+		String password = registration.getPassword();
+
+		if ((login.length()) > 50) {
+			errors.rejectValue("login", "lengthOfUser.registration.login",
+					"User Name must not more than 50 characters.");
+		}
+
+		if (password.length() < 4) {
+			errors.rejectValue("password", "Size.registration.password",
+					"User Name must not more than 50 characters.");
+		}
+
+	}
 }

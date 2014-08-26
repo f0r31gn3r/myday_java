8c84130823dc141d33c64653494b02eb17e1a150
Jevgenijs_Mokans <Jevgenijs.Mokans@gmail.com>
2014-08-26 13:34:58 +0300

    Minor fixes

--- MyDay/src/main/java/lv/localhost/MyDay/Controllers/PostController.java
+++ MyDay/src/main/java/lv/localhost/MyDay/Controllers/PostController.java
@@ -17,38 +17,37 @@ public class PostController {
 
 	private Post p;
 
-	@RequestMapping(value="/new_post", method = RequestMethod.POST)
-	public String createPost(Model model, HttpServletRequest request, HttpSession session) throws DBException{
-	
+	@RequestMapping(value = "/new_post", method = RequestMethod.POST)
+	public String createPost(Model model, HttpServletRequest request,
+			HttpSession session) throws DBException {
+
 		p = new Post();
-		p.setAuthorID( Integer.parseInt( request.getParameter("authorID") ));
+		p.setAuthorID(Integer.parseInt(request.getParameter("authorID")));
 		p.setTitle(request.getParameter("title"));
-		p.setBody( request.getParameter("body") );
-		
+		p.setBody(request.getParameter("body"));
+
 		PostDAOImpl i = new PostDAOImpl();
 		i.createPost(p);
-		
+
 		return "redirect:?page=posts";
-		
+
 	}
-	
-	
-	@RequestMapping(value="/post_edit", method = RequestMethod.POST)
-	public String editPost(Model model, HttpServletRequest request, HttpSession session) throws DBException{
-		
-		System.out.println("\n" + request.getParameter("postPostID"));
-			PostDAOImpl pi = new PostDAOImpl();
-			p = new Post();
-			p.setAuthorID(Integer.parseInt(request.getParameter("postAuthID")));
-			p.setTitle(request.getParameter("postTitle"));
-			p.setBody(request.getParameter("postBody"));
-			p.setPostID(Integer.parseInt(request.getParameter("postPostID")));
-			
-			pi = new PostDAOImpl();
-			pi.updatePost(p);
-			
-		
+
+	@RequestMapping(value = "/post_edit", method = RequestMethod.POST)
+	public String editPost(Model model, HttpServletRequest request,
+			HttpSession session) throws DBException {
+
+		PostDAOImpl pi = new PostDAOImpl();
+		p = new Post();
+		p.setAuthorID(Integer.parseInt(request.getParameter("postAuthID")));
+		p.setTitle(request.getParameter("postTitle"));
+		p.setBody(request.getParameter("postBody"));
+		p.setPostID(Integer.parseInt(request.getParameter("postPostID")));
+
+		pi = new PostDAOImpl();
+		pi.updatePost(p);
+
 		return "redirect:?page=post&id=" + p.getPostID();
-		
+
 	}
 }

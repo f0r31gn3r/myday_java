580ea46c3c14017fdbdd6c71020e4878d46b101a
Kiril_Janchev <you@example.com>
2014-08-26 15:42:44 +0300

    Definitly not an example!

--- MyDay/src/main/java/lv/localhost/MyDay/common/SpringException.java
+++ MyDay/src/main/java/lv/localhost/MyDay/common/SpringException.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class SpringExceptionExample {
+public class SpringException {
 	@ExceptionHandler(ResourceNotFoundException.class)
     public String handleResourceNotFoundException() {
         return "notfound";
